ab4ffbbaa5518a6dfec4028c63ff56c6af15bbae
AjxLab <abe12@mccc.jp>
2020-01-30 14:57:29 +0900

    1秒間入力しなければ次の文字へ

--- infer.py
+++ infer.py
@@ -6,7 +6,8 @@ from lib.predict import LSTM_
 infer = LSTM_(train=False)
 config = yaml.load(open('config/param.yaml'), Loader=yaml.SafeLoader)
 
+hundler = Hundle(config['size'])
 while True:
-    print('\n\n===== キーを入力してください ======')
-    data = input_keys(config['size'])
+    print('\n===== キーを入力してください ======')
+    data = hundler.input_keys()
     print('入力文字：%s' % infer.predict(data=data)[0])
--- lib/keyhundle/keyhundler.py
+++ lib/keyhundle/keyhundler.py
@@ -1,18 +1,33 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import time
 from getch import getch
 
 
-def input_keys(size):
-    ## -----*----- 連続キーを入力 -----*----- ##
-    keys = np.zeros(size)
+class Hundle():
+    def __init__(self, size):
+        ## -----*----- コンストラクタ -----*----- ##
+        self.size = size
+        self.reset()
 
-    for i in range(size):
-        key = getch()
-        keys[i] = int(key.encode('utf-8', 'replace').hex(), 16)
 
-        # 入力終了
-        if key == '\n':
-            break
+    def reset(self):
+        ## -----*----- ステータスをリセット -----*----- ##
+        self.keys = np.zeros(self.size)
+        self.is_end = False
 
-    return keys
+
+    def input_keys(self, limit=1.0):
+        ## -----*----- 連続キーを入力 -----*----- ##
+        self.reset()
+        t = time.time()
+
+        for i in range(self.size):
+            if time.time() - t > 1.0 and i >= 3:
+                break
+            t = time.time()
+
+            key = getch()
+            self.keys[i] = int(key.encode('utf-8', 'replace').hex(), 16)
+
+        return self.keys
